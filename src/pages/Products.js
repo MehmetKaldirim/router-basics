a286f1d884bd0b377f7a2b64bb343b5ab996b5b6
MehmetKaldirim <mathdenizi@gmail.com>
2024-02-28 20:16:32 +0100

    dynamic routes usage

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,3 +1,11 @@
 export default function ProductsPage(){
-    return <h1>The Products Page</h1>
+    return <>
+    <h1>The Products Page</h1>
+    <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+        <li>Product 4</li>
+    </ul>
+    </>
 }
